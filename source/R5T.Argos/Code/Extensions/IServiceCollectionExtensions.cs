7e1f79231e37cff6e3c51d5ce88a8fe49932ef15
MinexAutomation <minex.automation@gmail.com>
2020-07-17 03:48:13 -0700

    Moved IServiceCollection extensions and changed file name.

---
+++ source/R5T.Argos/Code/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+using R5T.Dacia;
+using R5T.Gimpolis;
+using R5T.Sardinia;
+
+
+namespace R5T.Argos
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="IOptions{TOptions}"/> of <see cref="GmailAuthentication"/> service.
+        /// </summary>
+        public static IServiceCollection AddGmailAuthentiation(this IServiceCollection services)
+        {
+            services.Configure<GmailAuthentication>();
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="IOptions{TOptions}"/> of <see cref="GmailAuthentication"/> service.
+        /// </summary>
+        public static IServiceAction<IOptions<GmailAuthentication>> AddGmailAuthentiationAction(this IServiceCollection services)
+        {
+            var serviceAction = ServiceAction.New<IOptions<GmailAuthentication>>(() => services.AddGmailAuthentiation());
+            return serviceAction;
+        }
+    }
+}
